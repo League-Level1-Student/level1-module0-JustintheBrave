4636dba9ec6092e651ef9eb192cbcf0e3f3f6e72
JustintheBrave <55635392+JustintheBrave@users.noreply.github.com>
2020-04-17 18:25:43 -0700

    drums

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -9,22 +9,30 @@ import org.jointheleague.graphical.robot.Robot;
 public class Houses {
 	static Robot house = new Robot();
 	public void run() {
-		house.miniaturize();
+		house.hide();
 		house.setSpeed(25);
 		house.setX(20);
 		house.setY(500);
 		house.penDown();
+		house.setWindowColor(0,0,0);
 		for (int i = 0; i < 10; i++) {
 			String tall = JOptionPane.showInputDialog("small medium or large");
 			String color = JOptionPane.showInputDialog("red, green, blue, or purple?");
-			extract(tall);
+			
+			if(tall.equalsIgnoreCase("large")) {
+				extract(tall, color);
+			}
+			
+			
+			else if(tall.equalsIgnoreCase("medium") || tall.equalsIgnoreCase("small")) {
+				drawPeakedRoof(tall, color);
+			}
 		}
 		
 	}
 	
 	static void extract(String height, String look) {
 		int tallness=0;
-		String colors;
 		if(height.equalsIgnoreCase("small")) {
 			tallness=60;
 		}
@@ -39,15 +47,112 @@ public class Houses {
 			house.setPenColor(255,0,0);
 		}
 		
+		else if(look.equalsIgnoreCase("green")) {
+			house.setPenColor(0,255,10);
+		}
+		
+		else if(look.equalsIgnoreCase("blue")) {
+			house.setPenColor(0,10,255);
+		}
+		
+		else if(look.equalsIgnoreCase("purple")) {
+			house.setPenColor(198,0,255);
+		}
 		
 		house.move(tallness);
 		house.turn(90);
 		house.move(20);
 		house.turn(90);
 		house.move(tallness);
+		house.setPenColor(35,255,104);
 		house.turn(-90);
 		house.move(20);
 		house.turn(-90);
 		
+	}
+	
+	static void drawPeakedRoof(String height, String look){
+		int tallness=0;
+		if(height.equalsIgnoreCase("small")) {
+			tallness=60;
+		}
+		else if(height.equalsIgnoreCase("medium")) {
+			tallness=120;
+		}
+		else if(height.equalsIgnoreCase("large")) {
+			tallness=250;
+		}
+		
+		if(look.equalsIgnoreCase("red")) {
+			house.setPenColor(255,0,0);
+		}
+		
+		else if(look.equalsIgnoreCase("green")) {
+			house.setPenColor(0,255,10);
+		}
+		
+		else if(look.equalsIgnoreCase("blue")) {
+			house.setPenColor(0,10,255);
+		}
+		
+		else if(look.equalsIgnoreCase("purple")) {
+			house.setPenColor(198,0,255);
+		}
+		
+		house.move(tallness);
+		house.turn(60);
+		house.move(15);
+		house.turn(60);
+		house.move(15);
+		house.turn(60);
+		house.move(tallness);
+		house.setPenColor(35,255,104);
+		house.turn(-90);
+		house.move(20);
+		house.turn(-90);
+		
+		
+	}
+	
+	
+	static void drawFlatRoof(String height, String look) {
+		int tallness=0;
+		if(height.equalsIgnoreCase("small")) {
+			tallness=60;
+		}
+		else if(height.equalsIgnoreCase("medium")) {
+			tallness=120;
+		}
+		else if(height.equalsIgnoreCase("large")) {
+			tallness=250;
+		}
+		
+		if(look.equalsIgnoreCase("red")) {
+			house.setPenColor(255,0,0);
+		}
+		
+		else if(look.equalsIgnoreCase("green")) {
+			house.setPenColor(0,255,10);
+		}
+		
+		else if(look.equalsIgnoreCase("blue")) {
+			house.setPenColor(0,10,255);
+		}
+		
+		else if(look.equalsIgnoreCase("purple")) {
+			house.setPenColor(198,0,255);
+		}
+		
+		house.move(tallness);
+		house.turn(90);
+		house.move(20);
+		house.turn(90);
+		house.move(tallness);
+		house.setPenColor(35,255,104);
+		house.turn(-90);
+		house.move(20);
+		house.turn(-90);
+		
+
 	}
 }
--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,35 +26,47 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		String patrick = "https://lh3.googleusercontent.com/proxy/m0aQOtQdxSIOmXUupsijkxmviY2RjTNMJJR4OsZjYVzmNT5w3MVXN-3n0ZFfVMoU1xDVJOyoa5UvY7-ZSTWE3_DrmFQBIIMXV8-hhu2K5Nr1hj3BiybPZ_EflGLsWZoJ9XU";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		image = createImage(patrick);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String ans = JOptionPane.showInputDialog("who is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(ans.equalsIgnoreCase("patrick")) {
+			System.out.println("Correct");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else {
+			System.out.println("Incorrect");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		Component pic;
+		String naruto = "https://pluspng.com/img-png/png-naruto-image-naruto-sage-mode-render-by-ahmedovicce-d4mqn5e-png-mangapanda-wiki-fandom-powered-by-wikia-900.png";
+		pic = createImage(naruto);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(pic);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String answ = JOptionPane.showInputDialog("who is this");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if(answ.equalsIgnoreCase("naruto")) {
+			System.out.println("Correct");
+		}
+		
+		else {
+			System.out.println("Incorrect");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
